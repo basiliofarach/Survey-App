5c0cc202e8f53c4794d03af38abd737f2d94516b
Roxasrojo <basiliofarach@yahoo.com.mx>
2021-01-07 01:28:38 -0600

    finished auth flow

---
+++ config/keys.js
@@ -0,0 +1,9 @@
+// Production credentials
+
+if (process.env.NODE_ENV === 'production') {
+  // Production - return prod keys
+  module.exports = require('./prod');
+} else {
+  // Development - return dev keys
+  module.exports = require('./dev');
+}
